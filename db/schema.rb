8c6a117c77f57f2ee8f7dc7628f674d195e06aec
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-11 17:13:05 +0100

    remove plugins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111154055) do
+ActiveRecord::Schema.define(:version => 20130111152448) do
 
   create_table "answers", :force => true do |t|
     t.datetime "created_at",    :null => false
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(:version => 20130111154055) do
   create_table "enonces", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "text"
   end
 
 end
