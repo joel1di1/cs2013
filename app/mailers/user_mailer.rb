a2a62be92100005d31d890b0b4abddcfcc6f8507
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-13 02:21:32 +0100

    best mail for me

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   default :from => 'joel1di1+codestory@gmail.com'
 
   def new_question(question)
-    mail :to => 'joel1di1@gmail.com', :subject => question
+    mail :to => 'joel1di1@gmail.com', :subject => "New Question from codestory : #{question}"
   end
   
 end
