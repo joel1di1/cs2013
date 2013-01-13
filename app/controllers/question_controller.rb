b8868d4a0a7b7316f27743995aa56c91c1e4a534
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-13 02:19:34 +0100

    respond to arithmetics questions

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -3,12 +3,16 @@ class QuestionController < ApplicationController
 	def show
 		question = params[:q]
 		if question
-			answer = Answer.find_by_question question
-			if answer.nil?
-				UserMailer.new_question(question).deliver
-				answer = Answer.create(:question => question, :answer_string => "je ne sais pas")
+			if request.query_string.match /(\d+)(([-+\/\*])(\d+))+/ 
+				render :text => eval(request.query_string)
+			else
+				answer = Answer.find_by_question question
+				if answer.nil?
+					UserMailer.new_question(question).deliver
+					answer = Answer.create(:question => question, :answer_string => "je ne sais pas")
+				end
+				render :text => answer.answer_string
 			end
-			render :text => answer.answer_string
 		else
 			render :text => 'Hello'	
 		end
