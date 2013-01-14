e8ad6a91653340a3b13dad8012d86668cd53cfd6
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-15 00:10:16 +0100

    test 1

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -1,7 +1,7 @@
 class JajascriptController < ApplicationController
 	
 	def optimize
-		render :json => Jajascript.optimize(request.params)
+		render :json => Jajascript.optimize(params.except(:controller, :action))
 	end
 
 end
