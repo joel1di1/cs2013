5cfe60486ba0212fda0035eb227cfc93ab750e54
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-17 21:37:03 +0100

    add logs params

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -1,6 +1,7 @@
 class JajascriptController < ApplicationController
 	
 	def optimize
+		p "optimize with -#{params}-"
 		res = Rails.cache.fetch params do
 			Jajascript.optimize(params.except(:controller, :action).first[0])
 		end
