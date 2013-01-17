c64d32ed1d3868b6b157fb9cdb3019f604826232
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-17 02:44:29 +0100

    emoe logs

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -1,9 +1,6 @@
 class JajascriptController < ApplicationController
 	
 	def optimize
-
-		p "Jajascript - #{params}"
-
 		res = Rails.cache.fetch params do
 			Jajascript.optimize(params.except(:controller, :action).first[0])
 		end
--- app/controllers/scalaskel_controller.rb
+++ app/controllers/scalaskel_controller.rb
@@ -1,9 +1,6 @@
 class ScalaskelController < ApplicationController
 	
 	def change
-
-		p "Scalaskel - #{params}"
-
 		res = Rails.cache.fetch params[:amount] do
 			Scalaskel.change(params[:amount].to_i)
 		end
