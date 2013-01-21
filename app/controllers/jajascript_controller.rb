4c5ecf0a3df5fb2eb4a34711981205228eaa838c
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-21 22:43:09 +0100

    remove log

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -1,7 +1,7 @@
 class JajascriptController < ApplicationController
 	
 	def optimize
-		p "optimize with -#{params}-"
+		# p "optimize with -#{params}-"
 		render :json =>	Jajascript.optimize(params.except(:controller, :action).first[0])
 	end
 
