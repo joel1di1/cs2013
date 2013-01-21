4345e666d7b4d2572b6cc78539f5d95d4dc1d5ce
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-21 16:30:12 +0100

    remove cache on jajascript

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -2,10 +2,7 @@ class JajascriptController < ApplicationController
 	
 	def optimize
 		p "optimize with -#{params}-"
-		res = Rails.cache.fetch params do
-			Jajascript.optimize(params.except(:controller, :action).first[0])
-		end
-		render :json => res
+		render :json =>	Jajascript.optimize(params.except(:controller, :action).first[0])
 	end
 
 end
