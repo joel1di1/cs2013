449101c743e7dfac154c6a8fa4c9c75e48e1414d
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-21 17:09:18 +0100

    arge params

--- config/initializers/rack_initializer.rb
+++ config/initializers/rack_initializer.rb
@@ -1,1 +1,1 @@
-Rack::Utils.key_space_limit = 1262144
+Rack::Utils.key_space_limit = 1000262144
--- spec/models/jajascript_spec.rb
+++ spec/models/jajascript_spec.rb
@@ -22,12 +22,18 @@ describe Jajascript do
 			it { Jajascript.optimize(@data).should eq( {gain: 20, path: ["AF1", "AF2"]} ) }
 		end
 
-		# describe 'test with lots of data' do
-		# 	before :each do 
-		# 		@data = " { \"VOL\": \"impossible-timer-57\", \"DEPART\": 0, \"DUREE\": 4, \"PRIX\": 7 }, { \"VOL\": \"relieved-housewife-92\", \"DEPART\": 1, \"DUREE\": 2, \"PRIX\": 3 }, { \"VOL\": \"plain-chrome-33\", \"DEPART\": 2, \"DUREE\": 6, \"PRIX\": 5 }, { \"VOL\": \"successful-cooking-16\", \"DEPART\": 4, \"DUREE\": 5, \"PRIX\": 21 }, { \"VOL\": \"colorful-shoeshine-68\", \"DEPART\": 5, \"DUREE\": 2, \"PRIX\": 1 }, { \"VOL\": \"thundering-fawn-73\", \"DEPART\": 5, \"DUREE\": 4, \"PRIX\": 15 }, { \"VOL\": \"pleasant-snowmobile-77\", \"DEPART\": 6, \"DUREE\": 2, \"PRIX\": 9 }, { \"VOL\": \"wicked-magnum-3\", \"DEPART\": 7, \"DUREE\": 6, \"PRIX\": 3 }, { \"VOL\": \"odd-scraper-76\", \"DEPART\": 9, \"DUREE\": 5, \"PRIX\": 19 }, { \"VOL\": \"amused-venom-68\", \"DEPART\": 10, \"DUREE\": 2, \"PRIX\": 27 }, { \"VOL\": \"outstanding-anchor-64\", \"DEPART\": 10, \"DUREE\": 4, \"PRIX\": 12 }, { \"VOL\": \"blushing-tractor-25\", \"DEPART\": 11, \"DUREE\": 2, \"PRIX\": 7 }, { \"VOL\": \"hissing-pod-14\", \"DEPART\": 12, \"DUREE\": 6, \"PRIX\": 6 }, { \"VOL\": \"disturbed-sometime-36\", \"DEPART\": 14, \"DUREE\": 5, \"PRIX\": 23 }, { \"VOL\": \"outstanding-quake-72\", \"DEPART\": 15, \"DUREE\": 2, \"PRIX\": 13 }, { \"VOL\": \"condemned-rubberstamp-87\", \"DEPART\": 15, \"DUREE\": 4, \"PRIX\": 12 }, { \"VOL\": \"muddy-testosterone-79\", \"DEPART\": 16, \"DUREE\": 2, \"PRIX\": 2 }, { \"VOL\": \"delightful-king-55\", \"DEPART\": 17, \"DUREE\": 6, \"PRIX\": 3 }, { \"VOL\": \"gentle-font-91\", \"DEPART\": 19, \"DUREE\": 5, \"PRIX\": 18 }, { \"VOL\": \"mammoth-sprinter-22\", \"DEPART\": 20, \"DUREE\": 2, \"PRIX\": 23 }, { \"VOL\": \"hungry-summertime-53\", \"DEPART\": 20, \"DUREE\": 4, \"PRIX\": 13 }, { \"VOL\": \"thundering-seismograph-18\", \"DEPART\": 21, \"DUREE\": 2, \"PRIX\": 8 }, { \"VOL\": \"helpless-treat-22\", \"DEPART\": 22, \"DUREE\": 6, \"PRIX\": 5 }, { \"VOL\": \"obedient-axle-29\", \"DEPART\": 24, \"DUREE\": 5, \"PRIX\": 18 }, { \"VOL\": \"charming-bud-37\", \"DEPART\": 25, \"DUREE\": 2, \"PRIX\": 27 }, { \"VOL\": \"repulsive-telegram-81\", \"DEPART\": 25, \"DUREE\": 4, \"PRIX\": 15 }, { \"VOL\": \"proud-termite-95\", \"DEPART\": 26, \"DUREE\": 2, \"PRIX\": 9 }, { \"VOL\": \"blushing-scholarship-82\", \"DEPART\": 27, \"DUREE\": 6, \"PRIX\": 1 }, { \"VOL\": \"beautiful-rattan-33\", \"DEPART\": 29, \"DUREE\": 5, \"PRIX\": 18 }, { \"VOL\": \"raspy-cowgirl-74\", \"DEPART\": 30, \"DUREE\": 2, \"PRIX\": 27 }, { \"VOL\": \"brainy-supermarket-77\", \"DEPART\": 30, \"DUREE\": 4, \"PRIX\": 13 }, { \"VOL\": \"ancient-roadside-42\", \"DEPART\": 31, \"DUREE\": 2, \"PRIX\": 1 }, { \"VOL\": \"bloody-cartoon-19\", \"DEPART\": 32, \"DUREE\": 6, \"PRIX\": 5 }, { \"VOL\": \"quaint-figurine-27\", \"DEPART\": 34, \"DUREE\": 5, \"PRIX\": 9 }, { \"VOL\": \"clumsy-mink-8\", \"DEPART\": 35, \"DUREE\": 2, \"PRIX\": 3 }, { \"VOL\": \"vast-kickoff-3\", \"DEPART\": 35, \"DUREE\": 4, \"PRIX\": 10 }, { \"VOL\": \"impossible-radish-3\", \"DEPART\": 36, \"DUREE\": 2, \"PRIX\": 8 }, { \"VOL\": \"wrong-wife-49\", \"DEPART\": 37, \"DUREE\": 6, \"PRIX\": 4 }, { \"VOL\": \"voiceless-vocabulary-26\", \"DEPART\": 39, \"DUREE\": 5, \"PRIX\": 8 }, { \"VOL\": \"hungry-babyhood-40\", \"DEPART\": 40, \"DUREE\": 2, \"PRIX\": 3 } "
-		# 	end
-		# 	it { Jajascript.optimize(@data).should eq( {gain: 20, path: ["AF1", "AF2"]} ) }
-		# end
+		describe 'test with lots of data' do
+			before :each do 
+				prng = Random.new(1234)
+				@data = []
+				max_node = 30000
+				(1..max_node).each do |i|
+					@data << {'VOL' => "node-#{i}", 'DEPART' => prng.rand(0..max_node), 'DUREE' => prng.rand(1..20), 'PRIX' => prng.rand(1..50)}
+				end
+				@data = @data.to_json
+			end
+			it { Jajascript.optimize(@data).should eq( {gain: 20, path: ["AF1", "AF2"]} ) }
+		end
 
 
 		describe 'first test' do 
