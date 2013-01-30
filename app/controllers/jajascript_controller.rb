ac94d9620d1b6bddf066d216ac5123fc1921ccfe
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-30 22:12:25 +0100

    remove mail

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -2,7 +2,6 @@ class JajascriptController < ApplicationController
 	
 	def optimize
 		# p "optimize with -#{params}-"
-		UserMailer.new_question(params).deliver
 		render :json =>	Jajascript.optimize(params.except(:controller, :action).first[0])
 	end
 
