08c363ba9673d803adb2c7d76b35e9ec62dc1969
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-30 22:16:14 +0100

    add mail

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -2,6 +2,7 @@ class JajascriptController < ApplicationController
 	
 	def optimize
 		# p "optimize with -#{params}-"
+		UserMailer.jajascript(params).deliver
 		render :json =>	Jajascript.optimize(params.except(:controller, :action).first[0])
 	end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   default :from => 'joel1di1+codestory@gmail.com'
 
   def jajascript(params)
-  	mail :to => 'joel1di1@gmail.com', :subject => "jajascript request"
+  	mail :to => 'joel1di1@gmail.com', :subject => "jajascript request", :text => params
   end
 
   def new_question(question)
