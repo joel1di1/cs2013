f6173e43e03029996175ac63bc2c6a665964abc9
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-30 22:18:09 +0100

    add mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   default :from => 'joel1di1+codestory@gmail.com'
 
   def jajascript(params)
-  	mail :to => 'joel1di1@gmail.com', :subject => "jajascript request", :text => params
+  	mail :to => 'joel1di1@gmail.com', :subject => "jajascript request", :text => params.to_s
   end
 
   def new_question(question)
