a6a38bb7a3b572f5fe7c1810100d1b1719d04a02
Benoit Lafontaine <joel1di1@gmail.com>
2013-01-31 01:16:23 +0100

    use other controller

--- app/controllers/jajascript_controller.rb
+++ app/controllers/jajascript_controller.rb
@@ -1,7 +1,15 @@
+require 'net/http'
+
 class JajascriptController < ApplicationController
 	
+	JJS_URI = URI('http://quiet-depths-3065.herokuapp.com/jajascript/optimize')
+
 	def optimize
 		# p "optimize with -#{params}-"
+
+		p params
+		res = Net::HTTP.post_form(JJS_URI, params.except(:action, :controller))
+
 		render :json =>	Jajascript.optimize(params.except(:controller, :action).first[0])
 	end
 
